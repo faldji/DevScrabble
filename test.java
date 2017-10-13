726d6ee9a2e5e80424bce97a5f190ce79b35f8f7
JohnsonN13 <32310543+JohnsonN13@users.noreply.github.com>
2017-10-13 09:04:15 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,14 @@
+package projet;
+
+/*import javax.swing.JFrame;
+import java.awt.Graphics;
+import javax.swing.JPanel;*/
+
+public class test {
+	public static void main(String[] args){
+		
+	    plateau1 plateau = new plateau1();
+	    //Plateau2 plateau2= new Plateau2();
+	    //plateau2.setVisible(true);
+	}
+}

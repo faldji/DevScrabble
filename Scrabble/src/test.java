b74c75c18372a53e9041cd0e89875dab48a5cd35
Faldji <Faldji@Faldji>
2017-10-13 10:37:21 +0200

    Config branche

---
+++ Scrabble/src/test.java
@@ -0,0 +1,13 @@
+
+/*import javax.swing.JFrame;
+import java.awt.Graphics;
+import javax.swing.JPanel;*/
+
+public class test {
+	public static void main(String[] args){
+		
+	    Plateau1 plateau = new Plateau1();
+	    //Plateau2 plateau2= new Plateau2();
+	    //plateau2.setVisible(true);
+	}
+}
